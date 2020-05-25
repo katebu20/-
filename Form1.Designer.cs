8ec28ac8e684330c95dc5241e0b5ef2e0c126cb2
katebu20 <65919926+katebu20@users.noreply.github.com>
2020-05-25 22:32:10 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.src = new System.Windows.Forms.RichTextBox();
+            this.adder = new System.Windows.Forms.RichTextBox();
+            this.rez = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // src
+            // 
+            this.src.Location = new System.Drawing.Point(26, 31);
+            this.src.Name = "src";
+            this.src.Size = new System.Drawing.Size(203, 243);
+            this.src.TabIndex = 0;
+            this.src.Text = "";
+            // 
+            // adder
+            // 
+            this.adder.Location = new System.Drawing.Point(264, 31);
+            this.adder.Name = "adder";
+            this.adder.Size = new System.Drawing.Size(174, 243);
+            this.adder.TabIndex = 1;
+            this.adder.Text = "";
+            // 
+            // rez
+            // 
+            this.rez.Location = new System.Drawing.Point(497, 31);
+            this.rez.Name = "rez";
+            this.rez.Size = new System.Drawing.Size(160, 243);
+            this.rez.TabIndex = 2;
+            this.rez.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 309);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(107, 309);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "del";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(188, 309);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "size";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(285, 309);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "writeToFile";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(375, 309);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "readFromFile";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(456, 309);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "contains";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(538, 308);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "join";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(619, 308);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "cross";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(700, 309);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 11;
+            this.button9.Text = "resume";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rez);
+            this.Controls.Add(this.adder);
+            this.Controls.Add(this.src);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox src;
+        private System.Windows.Forms.RichTextBox adder;
+        private System.Windows.Forms.RichTextBox rez;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        class MySet
+        {
+            public List<Object> mySet = new List<object>();
+
+            public List<Object> getAsList()
+            {
+                return mySet;
+            }
+            public void add(Object obj)
+            {
+                if (!mySet.Contains(obj))
+                    mySet.Add(obj);
+            }
+
+            public void del(Object obj)
+            {
+                mySet.Remove(obj);
+            }
+            public int size()
+            {
+                return mySet.Count;
+            }
+            public void writeToFile(String way)
+            {
+                using (StreamWriter writetext = new StreamWriter("write.txt"))
+                {
+                    foreach (var VARIABLE in mySet)
+                    {
+                        writetext.WriteLine(VARIABLE);
+
+                    }
+                }
+            }
+
+            public void readFromFile(String way)
+            {
+                String[] tmp = File.ReadAllLines(way);
+                foreach (var VARIABLE in tmp)
+                {
+                    mySet.Add(VARIABLE);
+                }
+            }
+
+            public bool contains(Object obj)
+            {
+                return mySet.Contains(obj);
+            }
+
+            public MySet cross(MySet crosser)
+            {
+                MySet rez = new MySet();
+                foreach (var VARIABLE in mySet)
+
+                {
+                    if (crosser.contains(VARIABLE))
+                        rez.add(VARIABLE);
+                }
+                return rez;
+            }
+
+            public MySet join(MySet joiner)
+            {
+                MySet rez = new MySet();
+                foreach (var VARIABLE in mySet)
+                {
+                    rez.add(VARIABLE);
+                }
+
+                foreach (var VARIABLE in joiner.getAsList())
+                {
+                    rez.add(VARIABLE);
+                }
+
+                return rez;
+            }
+
+            public MySet resume(MySet resumer)
+            {
+                MySet rez = new MySet();
+                foreach (var VARIABLE in mySet)
+                {
+                    rez.add(VARIABLE);
+                }
+                foreach (var VARIABLE in resumer.getAsList())
+                {
+                    if (rez.contains(VARIABLE))
+                        rez.del(VARIABLE);
+                }
+
+                return rez;
+            }
+
+
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rez.ResetText();
+            MySet mySet = new MySet();
+            String[] strs = src.Lines;
+            foreach (String str in strs)
+            {
+                mySet.add(str);
+            }
+            mySet.add(adder.Lines[0]);
+            foreach (Object s in mySet.mySet)
+            {
+                rez.AppendText(s.ToString()+"\n");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            rez.ResetText();
+            MySet mySet = new MySet();
+            String[] strs = src.Lines;
+            foreach (String str in strs)
+            {
+                mySet.add(str);
+            }
+            mySet.del(adder.Lines[0]);
+            foreach (Object s in mySet.mySet)
+            {
+                rez.AppendText(s.ToString() + "\n");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            rez.ResetText();
+            MySet mySet = new MySet();
+            String[] strs = src.Lines;
+            foreach (String str in strs)
+            {
+                mySet.add(str);
+            }
+            rez.AppendText(mySet.size().ToString() + "\n");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            rez.ResetText();
+            MySet mySet = new MySet();
+            String[] strs = src.Lines;      //todo fix
+            foreach (String str in strs)
+            {
+                mySet.add(str);
+            }
+            mySet.writeToFile(adder.Lines[0]);
+            using (System.IO.FileStream fs = new System.IO.FileStream(adder.Lines[0], FileMode.Append, FileAccess.Write))
+            {
+                foreach(Object o in mySet.mySet)
+                {
+                    fs.Write(Encoding.ASCII.GetBytes(o.ToString()+"\n"), 0, o.ToString().Length);
+                }
+                
+                fs.Flush();
+                fs.Close();
+            }
+            rez.AppendText("Done");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            rez.ResetText();
+            MySet mySet = new MySet();
+            mySet.readFromFile(adder.Lines[0]);
+            foreach (Object s in mySet.mySet)
+            {
+                rez.AppendText(s.ToString() + "\n");
+            }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            rez.ResetText();
+            MySet mySet = new MySet();
+            String[] strs = src.Lines;
+            foreach (String str in strs)
+            {
+                mySet.add(str);
+            }
+            rez.AppendText(mySet.contains(adder.Lines[0]).ToString() + "\n");
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+            rez.ResetText();
+            MySet mySet = new MySet();
+            String[] strs = src.Lines;
+            foreach (String str in strs)
+            {
+                mySet.add(str);
+            }
+            MySet mySet2 = new MySet();
+            foreach (String str in adder.Lines)
+            {
+                mySet2.add(str);
+            }
+            
+            foreach (Object s in mySet.join(mySet2).mySet)
+            {
+                rez.AppendText(s.ToString() + "\n");
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            rez.ResetText();
+            MySet mySet = new MySet();
+            String[] strs = src.Lines;
+            foreach (String str in strs)
+            {
+                mySet.add(str);
+            }
+            MySet mySet2 = new MySet();
+            foreach (String str in adder.Lines)
+            {
+                mySet2.add(str);
+            }
+
+            foreach (Object s in mySet.resume(mySet2).mySet)
+            {
+                rez.AppendText(s.ToString() + "\n");
+            }
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            rez.ResetText();
+            MySet mySet = new MySet();
+            String[] strs = src.Lines;
+            foreach (String str in strs)
+            {
+                mySet.add(str);
+            }
+            MySet mySet2 = new MySet();
+            foreach (String str in adder.Lines)
+            {
+                mySet2.add(str);
+            }
+
+            foreach (Object s in mySet.cross(mySet2).mySet)
+            {
+                rez.AppendText(s.ToString() + "\n");
+            }
+
+        }
+    }
+}
